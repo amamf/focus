c82780d52f66a2ff5092e19a75525b1807f9f011
amamf <amamf@users.noreply.github.com>
2016-11-20 03:18:00 +0300

    added initial versions of clock & tasks component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { ClockComponent } from './clock/clock.component';
+import { TasksComponent } from './tasks/tasks.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClockComponent,
+    TasksComponent
   ],
   imports: [
     BrowserModule,
