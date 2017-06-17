3343ba3908300b2db9d17cdc9e4bf15598804c8a
amamf <amamf@users.noreply.github.com>
2017-06-17 04:18:54 +0300

    create a new ng4-based version

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,14 @@
 import { FocusPage } from './app.po';
 
-describe('focus App', function() {
+describe('focus App', () => {
   let page: FocusPage;
 
   beforeEach(() => {
     page = new FocusPage();
   });
 
-  it('should display message saying app works', () => {
+  it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('app works!');
+    expect(page.getParagraphText()).toEqual('Welcome to app!!');
   });
 });
